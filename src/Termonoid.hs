911a92ea6f6dfc199e101b54e4dc35c67b51c210
pscollins <pscollins@uchicago.edu>
2015-06-02 05:20:14 -0500

    experiments

--- src/Termonoid.hs
+++ src/Termonoid.hs
@@ -1,3 +1,28 @@
 module Termonoid where
 
-main = undefined
+import Graphics.UI.Gtk
+import System.IO
+import Control.Monad.IO.Class
+
+main :: IO ()
+main = do
+  initGUI >>= print
+
+  win <- windowNew
+  f <- openFile "foo" WriteMode
+
+  -- let print = hPutStrLn f
+
+  widgetShowAll win
+
+-- THESE DONT DO ANYTHING, DONT KNOW WHY
+  -- on win mapSignal (print "hello world")
+  -- on win showSignal (print "hello world")
+  -- on win focus (\_ -> print "hello world" >> return True)
+
+  win `on` buttonPressEvent $ do
+    liftIO $ print "hi"
+    return True
+
+  mainGUI
+  mainQuit
