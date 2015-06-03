f2fa1520225ad52a576193bfdfefefc0467b3992
pscollins <pscollins@uchicago.edu>
2015-06-03 04:30:22 -0500

    here we go, at last

--- src/Termonoid.hs
+++ src/Termonoid.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE StandaloneDeriving #-}
 module Termonoid where
 
 import System.Posix.Pty
@@ -29,6 +30,8 @@ kvToS kv = case keyToChar kv of
             Nothing -> []
 
 
+type PtyOut = Either [PtyControlCode] ByteString
+
 type EventSource a = (AddHandler a, a -> IO ())
 
 addHandler :: EventSource a -> AddHandler a
@@ -38,7 +41,7 @@ fire :: EventSource a -> a -> IO ()
 fire = snd
 
 
-setupNetwork :: EventSource KeyVal -> EventSource ByteString -> IO EventNetwork
+setupNetwork :: EventSource KeyVal -> EventSource PtyOut -> IO EventNetwork
 setupNetwork keyPress textIn = compile $ do
   ePressed <- fromAddHandler $ addHandler keyPress
   eText <- fromAddHandler $ addHandler textIn
@@ -47,18 +50,45 @@ setupNetwork keyPress textIn = compile $ do
   reactimate $ fmap print eText
 
 
-watch :: EventSource ByteString -> Pty -> IO ()
+watch :: EventSource PtyOut -> Pty -> IO ()
 watch textIn pty = forever $ do
-  got <- readPty pty
+  got <- tryReadPty pty
+  fire textIn  got
+  threadWaitReadPty pty
   return ()
-  -- fire textIn got
+
+
+deriving instance Show BaudRate
+-- deriving instance Show (Either [PtyControlCode] ByteString)
+
+data Attributes = Attributes { inSpeed :: BaudRate
+                             , outSpeed :: BaudRate
+                             , bitsInByte :: Int
+                             , inTime :: Int
+                             , minIn :: Int } deriving Show
+                             -- , termMode :: TerminalMode}
+
+getAttributes :: TerminalAttributes -> Attributes
+getAttributes = Attributes <$>
+                inputSpeed <*>
+                outputSpeed <*>
+                bitsPerByte <*>
+                inputTime <*>
+                minInput
+
 
 main = do
   (pty, _) <-
-    spawnWithEnv "ls" [] (80, 80)
+    spawnWithEnv "bash" [] (80, 80)
 
   getTerminalName pty >>= print
   getSlaveTerminalName pty >>= print
+  getTerminalAttributes pty >>= print . getAttributes
+  attrs <- getTerminalAttributes pty
+
+  setTerminalAttributes pty (withMinInput (withTime attrs 0) 0) Immediately
+
+  getTerminalAttributes pty >>= print . getAttributes
 
   initGUI >>= print
 
