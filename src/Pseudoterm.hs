0115de09de2c7aed0352aaa1a822e75771dcfc91
pscollins <pscollins@uchicago.edu>
2015-06-04 08:48:20 -0500

    Revert "kill"
    This reverts commit 5f8241ce0cdc3d636661c003e54a2cf5245e879a.

---
+++ src/Pseudoterm.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE StandaloneDeriving, FlexibleInstances #-}
+module Pseudoterminal w3here
+
+import System.Posix.Pty
+import System.Process
+import Data.ByteString.Char8 (pack)
+
+-- | Spawn in my regular env
+spawnWithEnv :: FilePath -> [String] ->
+                (Int, Int) -> IO (Pty, ProcessHandle)
+spawnWithEnv = spawnWithPty Nothing True
+
+deriving instance Show BaudRate
+-- deriving instance Show (Either [PtyControlCode] ByteString)
+
+data Attributes = Attributes { inSpeed :: BaudRate
+                             , outSpeed :: BaudRate
+                             , bitsInByte :: Int
+                             , inTime :: Int
+                             , minIn :: Int } deriving Show
+                             -- , termMode :: TerminalMode}
+
+getAttributes :: TerminalAttributes -> Attributes
+getAttributes = Attributes <$>
+                inputSpeed <*>
+                outputSpeed <*>
+                bitsPerByte <*>
+                inputTime <*>
+                minInput
+
+main :: IO ()
+main = do
+  (pty, shellHandle) <-
+    spawnWithEnv "bash" [] (20, 10)
+
+  getTerminalName pty >>= print
+  getSlaveTerminalName pty >>= print
+
+  tryReadPty pty >>= print
+  writePty pty $ pack "ls\n"
+  tryReadPty pty >>= print
+  tryReadPty pty >>= print
+  tryReadPty pty >>= print
+  tryReadPty pty >>= print
+  tryReadPty pty >>= print
+  tryReadPty pty >>= print
+  tryReadPty pty >>= print
+
+  attrs <- getTerminalAttributes pty >>= return . getAttributes
+  print $ attrs
